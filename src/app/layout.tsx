8b0ed6cf8f5efa6425fdc3844904a7dca2e0fbdb
main <main@OSGOS-OSOSGOS.unassigned-domain>
2025-04-17 17:17:44 +0500

    аналитика

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { SpeedInsights } from '@vercel/speed-insights/next';
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/next";
 export const metadata: Metadata = {
   title: "Fitchecker",
 };
@@ -15,6 +16,7 @@ export default function RootLayout({
       <body>
         {children}
         <SpeedInsights />
+        <Analytics/>
       </body>
     </html>
   );
